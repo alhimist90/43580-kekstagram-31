e2f9bbfdc8298315808d6568a4de03fa0c4188cb
MrHwatz <alhimist90@yandex.ru>
2024-12-12 13:28:35 +0700

    Разбивает файлы JS на модули

---
+++ js/util.js
@@ -0,0 +1,27 @@
+const getRandomInteger = (minValue, maxValue) => {
+  const lower = Math.ceil(Math.min(Math.abs(minValue), Math.abs(maxValue)));
+  const upper = Math.floor(Math.max(Math.abs(minValue), Math.abs(maxValue)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomIdFromRange = (minValue, maxValue) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(minValue, maxValue);
+    if (previousValues.length >= (maxValue - minValue + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(minValue, maxValue);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+export {getRandomInteger, getRandomIdFromRange, getRandomArrayElement};
